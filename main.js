fb14234737249fb49bf212d136c90b81384e3b29
Ojas-Chowdhary <108580011+Ojas-Chowdhary@users.noreply.github.com>
2023-11-08 19:46:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+    download = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
+}
+
+function setup() {
+    canvas = createCanvas(350, 350);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(350,350)
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function draw() {
+    image(video, 0, 0, 350, 350);
+    image(download, noseX -14,  noseY, 30, 30);
+}
+
+function take_snapshot() {
+    save('My Name is John Cena.png')
+}
